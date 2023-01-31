5174478497051465c93ff674f86cf5d24430c3ad
jadashi97 <njadauniversal23@yahoo.com>
2023-01-31 08:34:25 -0600

    (fix): fixed pics issue. Now show the exact pics and add the links to projects

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -7,22 +7,22 @@ const projects = [
     {
         title: "Expense Tracker",
         description: "A handy tracker to help me track my everyDay expenses.Built using React for the frontend and nodejs for the backend",
-        getImageSrc:() => ("../assets/myAvatar.jpeg"),
+        getImageSrc: () => ("src/assets/lokose-passport-size.jpg")
     },
     {
         title: "World Volcano Tracker",
         description: "Used React and integrated the Nasa API to show erupting volcanoes around the world",
-        getImageSrc:  () => ("../assets/myAvatar.jpeg"),
+        getImageSrc:  () => ("src/assets/myAvatar.jpeg"),
     },
     {
         title: "Restaurant App",
         description: "Built a Mobile restaurant App using React Native for customer to easily look at the menu",
-        getImageSrc: () => ("../assets/giving.jpg"),
+        getImageSrc: () => ("src/assets/chillies.png"),
     },
     {
         title: "Church Website",
         description: "Built a Church Website for my community Church So we can show case any happening events",
-        getImageSrc: () => ("../assets/giving.jpg"),
+        getImageSrc: () => ("src/assets/afrifood.png"),
     }
 ]
 
@@ -50,7 +50,7 @@ const ProjectSection = () => {
                         key={project.title} 
                         title={project.title} 
                         description={project.description} 
-                        url="https://github.com/Jadashi97" 
+                        href= {"https://github.com/Jadashi97"}
                         imageSrc={project.getImageSrc()} 
                     /> 
                 ))} 
