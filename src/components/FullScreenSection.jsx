1b70e5550b0a50f4ac0ed61e9b4976cd82a64a43
jadashi97 <njadauniversal23@yahoo.com>
2023-03-23 20:25:38 -0500

    modify the landing page

--- src/components/FullScreenSection.jsx
+++ src/components/FullScreenSection.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { VStack } from "@chakra-ui/react";
+import { VStack, Flex, useMediaQuery} from "@chakra-ui/react";
 
 /** 
 * Illustrates the use of children prop and spread operator 
 */
 
+
 const FullScreenSection = ({ children, isDarkBackground, ...boxProps }) => {
+
+    // const [isNotSmallerScreen] = useMediaQuery("(min-width: 600px)");
+
     return(
         <VStack
             backgroundColor={boxProps.backgroundColor}
             color={isDarkBackground ? "white" : "black"}
         >
-            <VStack maxWidth="1280px" minHeight="100vh" {...boxProps}>
+            <VStack maxWidth="1280px" minHeight='100vh' {...boxProps}>
                 {children}
             </VStack>
         </VStack>
--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -1,40 +1,48 @@
 import React from 'react';
-import { Avatar, Heading, VStack } from '@chakra-ui/react';
+import { Image, Avatar, Heading, VStack, Text} from '@chakra-ui/react';
 import FullScreenSection from "./FullScreenSection";
+import { useMediaQuery } from '@chakra-ui/react';
 // import myAvatar from "../assets/myAvatar.jpeg";
 import AvatarPic from "../assets/AvatarPic.jpeg";
 
 const greeting = "Hello, I am Nyarji Daniel Jada";
-const bio1 = "A FullStack Developer";
+const bio1 = "A FullStack Developer inclined more onto the FrontEnd";
 const bio2 = "Specialized in Javascript,React & React Native, Express";
 
 
 const LandingSection = ()=> {
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+
     return(
        <FullScreenSection
             justifyContent="center"
-            alignItems="center"
+            alignItems="center" 
             isDarkBackground
-            backgroundColor="#2A4365"
+            backgroundColor="#202730"
        >
         <VStack spacing={16}>
             <VStack spacing={4} alignItems="center">
-                <Avatar
+            <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
+                    mb={isNotSmallerScreen ? "0" : "12"} borderRadius='full'
+                    backgroundColor="transparent" boxShadow="lg"
+                    boxSize="270px" src= {AvatarPic} />
+                {/* <Avatar
                     src={AvatarPic}
                     size="2xl"
                     name='lokose'
-                />
-                <Heading as="h4" size="md" noOfLines={1}>
+                /> */}
+                <Heading fontSize="5xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip='text'>
                     {greeting}
                 </Heading>
             </VStack>
             <VStack spacing={6}>
-                <Heading as="h1" size="3xl" noOfLines={1}>
+                <Heading as="h1" size="2xl" noOfLines={1}>
                     {bio1}
                 </Heading>
-                <Heading as="h2" size="2xl" noOfLines={2}>
+                <Text as="h2" size="2xl" noOfLines={2}>
                     {bio2}
-                </Heading>
+                </Text>
             </VStack>
         </VStack>
        </FullScreenSection>
