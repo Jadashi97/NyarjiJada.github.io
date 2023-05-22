7612e9867bcf69b8f650b1591c5d1f29101e22a9
jadashi97 <njadauniversal23@yahoo.com>
2023-05-22 16:17:37 -0500

    (feat): add new projects and images. (fix): solve the contact page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,44 +2,44 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { Heading, HStack, VStack, Button, Text, Link } from "@chakra-ui/react";
-import Icon from '@chakra-ui/icon';
-import { useMediaQuery } from '@chakra-ui/react';
+import Icon from "@chakra-ui/icon";
+import { useMediaQuery } from "@chakra-ui/react";
 
+const Card = ({ title, description, myIcon, imageSrc, linkSrc }) => {
+  const [isNotSmallerScreen] = useMediaQuery("(min-width:200px)");
 
-const Card = ({ title, description, myIcon, imageSrc, linkSrc, }) => {
-
-    const [isNotSmallerScreen] = useMediaQuery("(min-width:400px)");
-
-    return(
-        <VStack
-            color="black"
-            backgroundColor="white"
-            boxShadow="lg"
-            cursor="pointer"
-            borderRadius="xl"
-            alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"}
-            mb={isNotSmallerScreen ? "0" : "12"} 
-            py={"2.5rem"}
-        >
-            {/* <Icon color="black" p="4" as={myIcon} w="24" h="24"/> */}
-            {/* <Image borderRadius="xl" src={imageSrc} alt={title}/> */}
-            <VStack spacing={4} p={2} alignItems="center">
-                <p>{myIcon}</p>
-                <HStack justifyContent="space-between" alignItems="center">
-                    <Heading as="h2" size="lg" alignItem="center">
-                        {title}
-                    </Heading>
-                </HStack>
-                <Text color="#64748b" fontSize="lg">
-                    {description}
-                </Text>
-            </VStack>
-                <HStack spacing={4} alignItems="center">
-                    <Link color='teal.900' href={linkSrc} alignItem="center" size="lg" ><Button>See More</Button></Link>
-                </HStack>
-        </VStack>
-    );
+  return (
+    <VStack
+      color="black"
+      backgroundColor="white"
+      boxShadow="lg"
+      cursor="pointer"
+      borderRadius="xl"
+      alignSelf="center"
+      mt={isNotSmallerScreen ? "0" : "12"}
+      mb={isNotSmallerScreen ? "0" : "12"}
+      py={"2.0rem"}
+    >
+      {/* <Icon color="black" p="4" as={myIcon} w="24" h="24"/> */}
+      {/* <Image borderRadius="xl" src={imageSrc} alt={title}/> */}
+      <VStack spacing={4} p={2} alignItems="center">
+        <p>{myIcon}</p>
+        <HStack justifyContent="space-between" alignItems="center">
+          <Heading as="h2" size="lg" alignItem="center">
+            {title}
+          </Heading>
+        </HStack>
+        <Text color="#64748b" fontSize="lg">
+          {description}
+        </Text>
+      </VStack>
+      <HStack spacing={4} alignItems="center">
+        <Link color="teal.900" href={linkSrc} alignItem="center" size="lg">
+          <Button colorScheme="blue">See More</Button>
+        </Link>
+      </HStack>
+    </VStack>
+  );
 };
 
 export default Card;
-
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,80 +1,81 @@
 import React from "react";
 import FullScreenSection from "./FullScreenSection";
-import { Box, Heading} from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 import Card from "./Card";
 import { GiVolcano, GiTakeMyMoney } from "react-icons/gi";
+import { RiNetflixFill } from "react-icons/ri";
+import { BsCurrencyExchange } from "react-icons/bs";
 import { MdOutlineFoodBank } from "react-icons/md";
 import { BiChurch } from "react-icons/bi";
-import {FcCalculator} from "react-icons/fc";
-
+import { FcCalculator } from "react-icons/fc";
 
 const projects = [
-    {
-        title: "Expense Tracker",
-        description: "A handy tracker to help me track my daily expenses.React for the frontend and nodejs for the backend",
-        myIcon: <GiTakeMyMoney color="green" size={120}/>,
-        // image: {ExpenseTracker},
-        // getImageSrc: () => ("/expensetracker.jpeg"),
-        // getImageSrc:{ExpenseTracker},
-        getlinkSrc: () => ("https://github.com/Jadashi97/personal-Expense-tracker")
-
-    },
-    {
-        title: "Tip Calculator",
-        description: "Built a Tip calculator that you can use to split the bill equally among a group of friends. Click the Link and try it yourself",
-        myIcon: <FcCalculator color="Green" size={120}/>,
-        // getImageSrc: () => ("/churchcPic.jpeg"),
-        getlinkSrc: () => ("https://replit.com/@Jadashi97/Tip-Calculator")
-    },
-    {
-        title: "Restaurant App",
-        description: "Built a Mobile restaurant App using React Native for customers to easily look at the menu and order easily",
-        myIcon: <MdOutlineFoodBank color="silver" size={120}/>,
-        // getImageSrc: () => ("/afrifood.jpeg"),
-        getlinkSrc: () => ("https://github.com/Jadashi97/Restaurant-app")
-
-    },
-    {
-        title: "Church Website",
-        description: "Built My Community Church Website so we can communicate and worship better and together as a community",
-        myIcon: <BiChurch color="blue" size={120}/>,
-        // getImageSrc: () => ("/churchcPic.jpeg"),
-        getlinkSrc: () => ("https://southsudanchurch.com/")
-    }
-    
-]
-
-
+  {
+    title: "Expense Tracker",
+    description:
+      "A handy tracker to help me track my daily expenses.React for the frontend and nodejs for the backend",
+    myIcon: <GiTakeMyMoney color="green" size={120} />,
+    getlinkSrc: () => "https://tracking-expenditures.netlify.app/",
+  },
+  {
+    title: "Tip Calculator",
+    description:
+      "Built a Tip calculator that you can use to split the bill equally among a group of friends. Click the Link and try it yourself",
+    myIcon: <FcCalculator color="Green" size={120} />,
+    getlinkSrc: () => "https://personal-tippincalc.netlify.app/",
+  },
+  {
+    title: "Netflix-Clone",
+    description:
+      "Using React and Firebase to cement the front end and also spark the idea of providing streaming services to my beloved south Sudanese",
+    myIcon: <RiNetflixFill color="red" size={120} />,
+    getlinkSrc: () => "https://sollywoood-netflixclone.netlify.app/",
+  },
+  {
+    title: "Currency Converter",
+    description:
+      "Built in React to learn how to integrate the free currency API and easily do currency conversion",
+    myIcon: <BsCurrencyExchange color="gold" size={120} />,
+    getlinkSrc: () => "https://rasulu-converter.netlify.app/",
+  },
+  {
+    title: "Church Website",
+    description:
+      "Built My Community Church Website so we can communicate and worship better and together as a community",
+    myIcon: <BiChurch color="silver" size={120} />,
+    getlinkSrc: () => "https://southsudanchurch.com/",
+  },
+];
 
 const ProjectSection = () => {
-    return(
-        <FullScreenSection
-            backgroundColor="#14532d"
-            isDarkBackground 
-            p={8}
-            spacing={8}
-        >
-            <Heading as="h1" id="projects-section" alignItems={"center"} >
-                Featured Projects
-            </Heading>
-            <Box 
-                display="grid" 
-                // gridTemplateColumns="repeat(1,minmax(4,2fr))" 
-                gridGap={10} 
-            > 
-                {projects.map((project) => ( 
-                    <Card 
-                        key={project.title} 
-                        title={project.title} 
-                        description={project.description} 
-                        myIcon={project.myIcon}
-                        linkSrc= {project.getlinkSrc()}
-                        // imageSrc={project.getImageSrc()} 
-                    /> 
-                ))} 
-            </Box> 
-        </FullScreenSection>
-    );
+  return (
+    <FullScreenSection
+      backgroundColor="#1f7842"
+      isDarkBackground
+      p={8}
+      spacing={8}
+    >
+      <Heading as="h1" id="projects-section" alignItems={"center"}>
+        Featured Projects
+      </Heading>
+      <Box
+        display="grid"
+        // gridTemplateColumns="repeat(1,minmax(4,2fr))"
+        gridGap={10}
+      >
+        {projects.map((project) => (
+          <Card
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            myIcon={project.myIcon}
+            linkSrc={project.getlinkSrc()}
+            // imageSrc={project.getImageSrc()}
+          />
+        ))}
+      </Box>
+    </FullScreenSection>
+  );
 };
 
 export default ProjectSection;
